eb4eeeb6e7fa2e56ad63ba9b25ac5282f8218e04
Dmitriy Galochkin <arenda.tehniki87@gmail.com>
2020-09-11 16:01:50 +0300

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,36 +1,59 @@
-const add = (accum, element) => accum + element;
-const mult = (accum, element) => accum * element;
-const sub = (accum, element) => accum - element;
-
-function reduceArray(array, cb, initial) {
-  'use strict';
-  let i;
-  let accum;
-  if(arguments.length >= 3) {
-    accum = initial;
-    i = 0;
+ // Write code under this line
+ class Storage {
+  constructor(goods){
+  this.items = goods;
   }
-  if(arguments.length === 2) {
-    accum = array[0];
-    i = 1;
+  
+  getItems(){
+  return this.items;
   }
-  for(i; i < array.length; i += 1) {
-    const element = array[i];
-    accum = cb(accum, element)
-    // Write code under this line
-    
+  
+  addItem(item){
+  this.items.push(item);
   }
-  return accum;
-}
+  
+  removeItem(item) {
+    this.items = this.items.filter((itemName) => itemName !== item);
+  }
+  
+  
+}    
 
-const arr  = [1,2,3,4,5];
+ console.log(typeof Storage);
+// 'function'
 
-console.log(reduceArray(arr, add)); // 15
-console.log(reduceArray(arr, add, 10)); // 25
+const goods = [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+];
 
-console.log(reduceArray(arr, mult)); // 120
-console.log(reduceArray(arr, mult, 10)); // 1200
+ const storage = new Storage(goods);
 
-console.log(reduceArray(arr, sub)); // -13
-console.log(reduceArray(arr, sub, 10)); // -5
- 
+ console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор'
+] */
+
+ storage.addItem('Дроид');
+ console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
+
+ storage.removeItem('Пролонгер');
+ console.log(storage.getItems());
+/* [
+  'Нанитоиды',
+  'Железные жупи',
+  'Антигравитатор',
+  'Дроид'
+] */
